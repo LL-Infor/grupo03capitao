9df6f3d71c9f78330933a5a68d07737be8fb1ec6
LL-Infor <leandebora@gmail.com>
2026-09-25 12:18:42 -0300

    Configuração Inicial do Projeto

---
+++ src/main/java/br/com/ll/grupo03capitao/Grupo03capitaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ll.grupo03capitao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo03capitaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo03capitaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ll/grupo03capitao/Grupo03capitaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ll.grupo03capitao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo03capitaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
